ec1328b2c8b3a0636b8efd0d51391aec45cacca0
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2024-09-22 16:48:12 -0300

    Crea login

---
+++ login.py
@@ -0,0 +1,30 @@
+def login():
+
+    usuarios = (
+        ("usuario1", "contrasenia1"),
+        ("usuario2", "contrasenia2"),
+        ("usuario3", "contrasenia3"),
+    )
+    intentos_permitidos = 3
+
+    print('¡Bienvenido!')
+    while intentos_permitidos > 0:
+        nombre_usuario = input("Introduce tu usuario: ")
+        contrasenia_usuario = input("Introduce tu contraseña: ")
+
+        for usuario, contrasenia in usuarios:
+            if usuario == nombre_usuario and contrasenia == contrasenia_usuario:
+                # Inicio de sesión exitoso
+                print("¡Acceso exitoso!")
+                print("-----------------------------------------------------------")
+                return True
+
+        # Disminuir el número de intentos
+        intentos_permitidos -= 1
+        print(
+            f"Credenciales incorrectas. Te quedan {intentos_permitidos} intento(s).")
+        print("-----------------------------------------------------------")
+
+    # Si no se logran las credenciales después de 3 intentos
+    print("Has agotado todos los intentos. Acceso denegado.")
+    return False
--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 from funciones import cargar_peliculas, buscar_por_genero, buscar_por_anio, mostrar_peliculas, buscar_por_descripcion
+from login import login
 
-# Cargar las películas desde el archivo JSON
-ruta_json = 'peliculas.json'
-peliculas = cargar_peliculas(ruta_json)
+if login():
 
-# Opciones del usuario
-print("Opciones de búsqueda:")
-print("1. Buscar por género")
-print("2. Buscar por rango de años")
-print("3. Buscar por descripción")
-opcion = int(input("Selecciona una opción: "))
+    # Cargar las películas desde el archivo JSON
+    ruta_json = 'peliculas.json'
+    peliculas = cargar_peliculas(ruta_json)
 
-if opcion == 1:
-    genero = input("Introduce el género: ")
-    resultado = buscar_por_genero(peliculas, genero)
-elif opcion == 2:
-    anio_inicio = int(input("Introduce el año de inicio: "))
-    anio_fin = int(input("Introduce el año final: "))
-    resultado = buscar_por_anio(peliculas, anio_inicio, anio_fin)
-elif opcion == 3:
-    palabras_clave = input(
-        "Introduce una breve descripción o palabras clave: ")
-    resultado = buscar_por_descripcion(peliculas, palabras_clave)
+    # Opciones del usuario
+    print("Opciones de búsqueda:")
+    print("1. Buscar por género")
+    print("2. Buscar por rango de años")
+    print("3. Buscar por descripción")
+    opcion = int(input("Selecciona una opción: "))
 
-# Mostrar los resultados
-if resultado:
-    mostrar_peliculas(resultado)
-else:
-    print("No se encontraron películas que coincidan con la búsqueda.")
+    if opcion == 1:
+        genero = input("Introduce el género: ")
+        resultado = buscar_por_genero(peliculas, genero)
+    elif opcion == 2:
+        anio_inicio = int(input("Introduce el año de inicio: "))
+        anio_fin = int(input("Introduce el año final: "))
+        resultado = buscar_por_anio(peliculas, anio_inicio, anio_fin)
+    elif opcion == 3:
+        palabras_clave = input(
+            "Introduce una breve descripción o palabras clave: ")
+        resultado = buscar_por_descripcion(peliculas, palabras_clave)
+
+    # Mostrar los resultados
+    if resultado:
+        mostrar_peliculas(resultado)
+    else:
+        print("No se encontraron películas que coincidan con la búsqueda.")
